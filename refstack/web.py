222d82dfb4702d38ea95d14f1013c4b1ab38f4de
Joshua McKenty <jmckenty@gmail.com>
2013-04-17 12:57:51 -0700

    Adding sqlite db for the data store.

--- refstack/web.py
+++ refstack/web.py
@@ -4,20 +4,31 @@ Simple Refstack website.
 """
 
 import os
-import sys
 import random
+import sqlite3
+import sys
 from flask import Flask, request, render_template, g, jsonify
-from twisted.internet import task
-from twisted.internet import reactor
-from twisted.web.wsgi import WSGIResource
-from twisted.web.server import Site
 
 
-PORT = 80
+# TODO(JMC): Make me a config var
+DATABASE = '/var/www/refstack/database.db'
+
 
 app = Flask(__name__)
 app.debug = True
 
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 @app.route('/', methods=['POST','GET'])
 def index():
