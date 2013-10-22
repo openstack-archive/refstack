ed4287a021193d4fc35c88ba02fcba3b74904ddc
David Lenwell <dlenwell@gmail.com>
2013-10-21 20:12:04 -0700

    added testr wrapping code

--- refstack/common/test.py
+++ refstack/common/test.py
@@ -16,6 +16,103 @@
 from refstack.common.tempest_config import TempestConfig
 
 
+"""Source for test results.
+
+This abstracts the various sources of test results that Tribunal can 
+read from.
+"""
+
+
+class UnreloadableStream(Exception):
+    """The test stream can not be reloaded (probably because it is a pipe)."""
+
+
+class NoStreamPresent(Exception):
+    """The TestSource does not contain any subunit streams."""
+
+
+class FileTestSource(object):
+    """Tests results read from a file.
+
+    Reloading re-reads the contents of the file.
+    """
+
+    def __init__(self, input_file):
+        """Make a new file testsource.
+
+        :param input_file: Open file containing test results.
+        """
+        fcntl.fcntl(input_file, fcntl.F_SETFL, os.O_NONBLOCK)
+        self.input_file = input_file
+        
+    def get_subunit_stream(self):
+        return self.input_file
+
+    def reload(self):
+        try:
+            self.input_file.seek(0)
+        except IOError, e:
+            sys.stderr.write("error seeking file: %s\n" % e)
+            if e.errno == errno.ESPIPE:
+                raise UnreloadableStream()
+            else:
+                raise
+        else:
+            return self.input_file
+
+
+class TestRepositorySource(object):
+    """Get test results from a testrepository.
+
+    Reloading asks testr to re-run (and implicitly record) a new 
+    test result.
+
+    :ivar testr_directory: path to directory containing .testr repository
+    """
+
+    def __init__(self, testr_directory):
+        # Work around bug in testrepository (just filed, no # yet).
+        self.testr_directory = os.path.expanduser(testr_directory)
+        
+    def get_subunit_stream(self):
+        try:
+            return self.testrepository_last_stream()
+        except KeyError:
+            raise NoStreamPresent()
+
+    def reload(self):
+        # run the test suite again.
+        from testrepository.commands import run
+        
+        cmd = run.run(ui)
+        # Work around 'testr run' loading to the wrong repo.
+        
+        here = os.getcwd()
+        os.chdir(self.testr_directory)
+        
+        try:
+            res = cmd.execute()
+        finally:
+            os.chdir(here)
+        # Ignoring detail of exit code: the subunit parsing + ui error
+        # methods will inform the user.
+        if res == 3:
+            # TODO: give a warning if execution failed
+            # But if it went badly wrong.. something 
+            return
+        return self.testrepository_last_stream()
+
+    def testrepository_last_stream(self):
+        import testrepository.repository.file
+        factory = testrepository.repository.file.RepositoryFactory()
+        repo = factory.open(self.testr_directory)
+        # this is poor because it just returns a stringio for the whole 
+        # thing; we should instead try to read from it as a file so we can 
+        # get nonblocking io
+        return repo.get_test_run(repo.latest_id()).get_subunit_stream()
+
+
+
 class Test(object):
     """ Test functions"""
     test_id = None
