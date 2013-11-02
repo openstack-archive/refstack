980d2e588762dcef7efae3c45aa6ba38612c39e0
David Lenwell <dlenwell@gmail.com>
2013-11-01 21:13:55 -0700

    added try

--- scripts/import_vendors.py
+++ scripts/import_vendors.py
@@ -5,9 +5,11 @@ _file = 'members_sponsors_20131030.csv'
 with open(_file, 'rb') as csvfile:
     parsed = csv.reader(csvfile, delimiter=',')
     for row in parsed:
-        if not row[3] == '':
+        try:
             vendor = Vendor()
             vendor.vendor_name = row[1]
             vendor.contact_email = row[3]
             db.add(vendor)
             db.commit()
+        except:
+            print 'vendor skipped:'+row[1]
