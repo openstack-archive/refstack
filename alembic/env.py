587d15ec2d5652f8f460ac0aaf2458ea1f663bdc
termie <github@anarkystic.com>
2013-12-05 23:48:37 -0800

    remove meta so alembic doesn't require refstack

--- alembic/env.py
+++ alembic/env.py
@@ -35,11 +35,9 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-# target_metadata = None
-from refstack.models import db
-target_metadata = db.metadata
+# from refstack.models import db
+# target_metadata = db.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
