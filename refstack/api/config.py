c76114ea23278d3d1539f8b78fb4991caca2f943
Vladislav Kuzmin <vkuzmin@mirantis.com>
2014-12-26 14:07:32 +0400

    Add API prototype on Pecan
    Add API prototype on Pecan.
    Add functioanal tests.
    Update documentation for running new API.
    Delete old API and unit tests for it.
    
    Change-Id: I42b3539d8420b2b5edf44d5265654b738e7ffa5e

---
+++ refstack/api/config.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2015 Mirantis, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Configuration for running API.
+
+Custom Configurations must be in Python dictionary format:
+
+foo = {'bar':'baz'}
+
+All configurations are accessible at:
+pecan.conf
+"""
+
+# Server Specific Configurations
+server = {
+    'port': '8000',
+    'host': '0.0.0.0'
+}
+
+# Pecan Application Configurations
+app = {
+    'root': 'refstack.api.controllers.root.RootController',
+    'modules': ['refstack.api'],
+    'static_root': '%(confdir)s/public',
+    'template_path': '%(confdir)s/${package}/templates',
+    'debug': False,
+    'errors': {
+        '404': '/error/404',
+        '__force_dict__': True
+    }
+}
+
+logging = {
+    'loggers': {
+        'root': {'level': 'INFO', 'handlers': ['console']},
+        'refstack': {'level': 'DEBUG', 'handlers': ['console']}
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'formatters': {
+        'simple': {
+            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                       '[%(threadName)s] %(message)s')
+        }
+    }
+}
