c7d4caa60953ca07533ec3ecdabb7f870c7fb840
Michael Krotscheck <krotscheck@gmail.com>
2015-06-01 11:39:59 -0700

    Added Chromium Tests
    This enables tests against the Chromium project for refstack's
    javascript components.
    
    Change-Id: Id2dfbee5c2c6e1e897933335b9cb4c18d2d7174a

--- refstack-ui/tests/karma.conf.js
+++ refstack-ui/tests/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
 
         frameworks: ['jasmine'],
 
-        browsers: ['Firefox', 'PhantomJS'],
+        browsers: ['Firefox', 'PhantomJS', 'Chrome'],
 
         plugins: [
             'karma-chrome-launcher',
